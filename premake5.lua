779e7383e5eb530531f3b0384eb983d716498fcb
Quentin Lurkin <qlurkin@gmail.com>
2020-07-24 23:30:57 +0200

    Update Premake File

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -19,9 +20,7 @@ project "GLFW"
 	}
 	filter "system:linux"
 		pic "On"
-
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
@@ -44,7 +43,6 @@ project "GLFW"
 
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 
 		files
 		{
